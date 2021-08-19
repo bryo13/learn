78a61d787715dea1e43d0ab90f0c4f6fc474d954
bryok13 <bryok2@gmail.com>
2021-08-19 21:18:30 +0300

    More comments

--- locate/path.go
+++ locate/path.go
@@ -11,6 +11,7 @@ import (
 type Files interface {
 	FindHome() (string, error)
 	MakeDir(string) (bool, error)
+	OpenFile(string) (*os.File, error)
 	SpecifyLocation(string, string) (string, string)
 }
 
@@ -27,25 +28,25 @@ func (l *LocateFiles) FindHome() (string, error) {
 }
 
 // first return string is dir location and the second is the file name plus location
-func (l *LocateFiles) SpecifyLocation(path, nm string) (string, string) {
+func (l *LocateFiles) SpecifyLocation(folderName, path, nm string) (string, string) {
 	locat, _ := l.FindHome()
-	path = fmt.Sprintf("%s/%s", locat, "glearn")
+	path = fmt.Sprintf("%s/%s", locat, folderName)
 	fileToUse := fmt.Sprintf("%s/%s", path, nm)
 
 	return path, fileToUse
 }
 
 // MakeDir take the location of the home dir and makes a folder if none exists or returns an error
-func (l *LocateFiles) MakeDir(home string) (bool, error) {
+func (l *LocateFiles) MakeDir(home, folderName string) (bool, error) {
 	home, _ = l.FindHome()
 	homedir, err := os.ReadDir(home)
-	dirName, _ := l.SpecifyLocation(home, "glearn.log")
+	dirName, _ := l.SpecifyLocation(folderName, home, "glearn.log")
 	if err != nil {
 		return false, errors.New("Could not read home dir")
 	}
 
 	for _, name := range homedir {
-		if name.Name() != "glearn" {
+		if name.Name() != folderName {
 			os.Mkdir(dirName, 0755)
 			fmt.Println("Created ")
 			return false, nil
@@ -53,4 +54,8 @@ func (l *LocateFiles) MakeDir(home string) (bool, error) {
 		break
 	}
 	return true, nil
+}
+
+func (l *LocateFiles) OpenFile(path string) {
+	// To open we need location to file
 }
--- logger/logs.go
+++ logger/logs.go
@@ -6,13 +6,21 @@ import (
 	"os"
 )
 
+// Logs defines a l of type *Log.logger
 type Logs struct {
 	l *log.Logger
 }
 
-func (lg *Logs) SaveLogs(file *os.File, msg string) error {
+// SaveLogs take the defination of Logs and saves it in a file
+func (lg *Logs) SaveLogs(path string, file *os.File, msg string) error {
+	// variables
 	lg.l = log.New(file, msg, log.Llongfile)
 
+	// Open folder to save logs
+	// Open file
+	// set location
+	// write log file
+	// defer close file
 	fmt.Println(lg.l)
 	return nil
 }
